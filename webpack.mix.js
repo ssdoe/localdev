2857edd8f4abebe5529dbb3a10b1338d9a7bd243
Said Salameh <ss@saidsalameh.com>
2022-09-19 17:11:46 +0800

    Megamenu update

--- webpack.mix.js
+++ webpack.mix.js
@@ -4,7 +4,7 @@ mix.disableNotifications()
 
 mix.js("src/js/app.js", "dist/")
     .sass("src/scss/app.scss", "dist/")
-    .sass("src/scss/custom.scss", "dist/");
+    .sass("src/scss/index.scss", "dist/");
 
 // Autoprefixer is enabled by default, see:
 // https://laravel-mix.com/docs/6.0/autoprefixer
